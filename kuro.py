2dbf27c2e7ccc050bbc9e7ba3bd8237fdc801290
Mikhail Serebryakov <soraenjoyer@mail.ru>
2022-12-20 16:06:08 +0500

    requirements.txt and pep8

--- kuro.py
+++ kuro.py
@@ -9,8 +9,10 @@ parser = argparse.ArgumentParser(description="UbiArt Wii texture encoder")
 
 parser.add_argument("-i", "--input", help="Input image", required=True)
 parser.add_argument("-o", "--output", help="Output texture")
-parser.add_argument("-w", "--wimgt-executable-path", default="wimgt.exe", help="Path to Wiimms Image Tool")
-parser.add_argument("-m", "--masked", action="store_true", help="Texture should be masked?")
+parser.add_argument("-w", "--wimgt-executable-path",
+                    default="wimgt.exe", help="Path to Wiimms Image Tool")
+parser.add_argument("-m", "--masked", action="store_true",
+                    help="Texture should be masked?")
 
 
 # PIL saves PNGs with sRGB profile what makes Wiimm Image Tool crash
@@ -22,17 +24,20 @@ def safe_save(img, output_path):
     srgb_profile = ImageCms.createProfile("sRGB")
 
     # Converting input image from original profile to clean sRGB profile
-    safe_img = ImageCms.profileToProfile(img, io.BytesIO(profile), srgb_profile)
+    safe_img = ImageCms.profileToProfile(
+        img, io.BytesIO(profile), srgb_profile)
 
     # Save .PNG image with clean sRGB profile
-    safe_img.save(output_path, icc_profile=ImageCms.ImageCmsProfile(srgb_profile).tobytes(), format="png")
+    safe_img.save(output_path, icc_profile=ImageCms.ImageCmsProfile(
+        srgb_profile).tobytes(), format="png")
 
 
 # Function for building Alpha Map Container for masked textures
 def build_apmc(writer_io, img, args):
     # Write header stuff
-    writer_io.write(b"APMC") # AlPha Map Container
-    writer_io.write(b"\0\0\0\x20\0\xFF\0\0\0\0\xFF\0\0\0\0\xFF\xFF\0\0\0\0\0\x10\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0")
+    writer_io.write(b"APMC")  # AlPha Map Container
+    writer_io.write(
+        b"\0\0\0\x20\0\xFF\0\0\0\0\xFF\0\0\0\0\xFF\xFF\0\0\0\0\0\x10\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0")
 
     # Creating the alpha & mask source textures
     alpha_texture = img.convert("RGB").convert("RGBA")
@@ -78,8 +83,9 @@ def build_1txd(writer_io, img, args):
     source_texture = "temp/source_texture.png"
 
     # Write the header stuff
-    writer_io.write(b"1TXD") # 1 TeXture (D???)
-    writer_io.write(b"\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\x10\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0")
+    writer_io.write(b"1TXD")  # 1 TeXture (D???)
+    writer_io.write(
+        b"\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\x10\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0")
 
     # Convert source texture to TEX format with CMPR encoding
     os.system(f"{args.wimgt_executable_path} ENCODE \"temp/source_texture.png\" --transform tex.cmpr --overwrite --strip --dest \"temp/destination_texture.tex\"")
@@ -92,7 +98,7 @@ def build_1txd(writer_io, img, args):
         # Getting the texture data size and read it
         f.seek(0x14, os.SEEK_SET)
         tex_size = struct.unpack(">I", f.read(4))[0]
-   
+
         f.seek(0x40, os.SEEK_SET)
         tex_data = f.read(tex_size)
 
@@ -103,6 +109,7 @@ def build_1txd(writer_io, img, args):
     # from temporary directory
     os.remove(source_texture)
     os.remove(destination_texture)
+
 
 def main():
     # Parse the command line arguments
@@ -116,7 +123,8 @@ def main():
     # Getting the information from command line arguments
     input_file = args.input
     masked = args.masked
-    output_file = args.output if args.output else os.path.splitext(args.input)[0] + ".tga.ckd"
+    output_file = args.output if args.output else os.path.splitext(args.input)[
+        0] + ".tga.ckd"
 
     # Open the image using PIL library
     img = Image.open(input_file)
@@ -124,7 +132,8 @@ def main():
 
     # Checks the image size
     if w % 2 != 0 or h % 2 != 0:
-        print("Bad image input. Width and height should be width and height with a power of 2")
+        print(
+            "Bad image input. Width and height should be width and height with a power of 2")
         sys.exit(-1)
 
     # Writing the UbiArt texture container header
@@ -134,18 +143,22 @@ def main():
     header_buf.write(b"\0\0\0\x09\x54\x45\x58\0\0\0\0\x2C\x00\x00\x80\x80")
 
     header_buf.write(struct.pack(">HH", w, h))
-    header_buf.write(b"\x00\x01\x18\0\0\0\x80\x80\0\0\0\0\0\x10\0\0\0\0\0\0\0\0\xCC\xCC")
+    header_buf.write(
+        b"\x00\x01\x18\0\0\0\x80\x80\0\0\0\0\0\x10\0\0\0\0\0\0\0\0\xCC\xCC")
 
     # Writing the contained texture header
     contained_buf = io.BytesIO()
     contained_buf.write(b"\x20\x53\x44\x44\0\0\0\x7C\0\x08\x10\x07")
     contained_buf.write(struct.pack(">II", w, h))
-    contained_buf.write(b"\0\0\x80\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\x54\x54\x56\x4E\0\x02\0\x07\0\0\0\x20\0\0\0\x04")
+    contained_buf.write(
+        b"\0\0\x80\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\x54\x54\x56\x4E\0\x02\0\x07\0\0\0\x20\0\0\0\x04")
 
     # If we need masked, then we will do APMC
     # If not, then 1TXD
-    if masked: build_apmc(contained_buf, img, args)
-    else: build_1txd(contained_buf, img, args)
+    if masked:
+        build_apmc(contained_buf, img, args)
+    else:
+        build_1txd(contained_buf, img, args)
 
     # Writing the contained data size to UbiArt header
     header_buf.seek(0xc, os.SEEK_SET)
